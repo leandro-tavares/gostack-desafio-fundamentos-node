067dc72ff943d6f7c6e44b952faab28b86a6861a
Leandro Tavares <leandro@phox.com.br>
2020-04-14 10:09:29 -0300

    Transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,3 +1,4 @@
+import { response } from 'express';
 import Transaction from '../models/Transaction';
 
 interface Balance {
@@ -9,20 +10,43 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private income: number;
+
+  private outcome: number;
+
+  private total: number;
+
   constructor() {
     this.transactions = [];
+    this.income = 0;
+    this.outcome = 0;
+    this.total = 0;
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    if (this.transactions && this.transactions.length > 0) {
+      const transactionIncome = this.transactions.map(item =>
+        item.type === 'income' ? item.value : 0,
+      );
+      const transactionOutcome = this.transactions.map(item =>
+        item.type === 'outcome' ? item.value : 0,
+      );
+
+      this.income = transactionIncome.reduce((total, val) => total + val);
+      this.outcome = transactionOutcome.reduce((total, val) => total + val);
+      this.total = this.income - this.outcome;
+    }
+    return { income: this.income, outcome: this.outcome, total: this.total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Transaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
